a777e62ee0025ad1746920691e99a7afc9c5b576
Sean Walsh <sean@dronedeploy.com>
2018-06-11 08:24:55 -0700

    clear text/move around

--- lineeditor.py
+++ lineeditor.py
@@ -14,4 +14,13 @@ class LineEditor(urwid.Edit):
             urwid.emit_signal(self, 'done', None)
             return
 
+        if key == 'ctrl l':
+            self.set_edit_text("")
+
+        if key == 'ctrl a':
+            self.set_edit_pos(0)
+
+        if key == 'ctrl e':
+            self.set_edit_pos(len(self.get_edit_text()))
+
         return urwid.Edit.keypress(self, size, key)
