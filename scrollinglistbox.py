c8c87f8449ae7573a52e740ece0d4bd397564267
Sean Walsh <sean@dronedeploy.com>
2018-06-11 11:45:49 -0700

    more scrolling options

--- scrollinglistbox.py
+++ scrollinglistbox.py
@@ -3,17 +3,34 @@ import urwid
 class ScrollingListBox(urwid.ListBox):
 
     def scroll_down(self):
-        focus = self.get_focus()[1]
-        if focus is not None:
-            try:
-                self.set_focus(focus + 1)
-            except:
-                return
+        self.down_by(1)
 
     def scroll_up(self):
-        focus = self.get_focus()[1]
-        if focus > 0:
-            self.set_focus(focus - 1)
+        self.up_by(1)
+
+    def page_down(self):
+        self.down_by(5)
+
+    def page_up(self):
+        self.up_by(5)
+
+    def top(self):
+        self.set_focus(0)
+
+    def bottom(self):
+        self.set_focus(len(self.body.positions()) - 1)
+
+    def down_by(self, n):
+        focus = self.focus_position or 0
+        new_focus = min(focus + n, len(self.body.positions()) - 1)
+
+        self.set_focus(new_focus)
+
+    def up_by(self, n):
+        focus = self.focus_position or 0
+        new_focus = max(focus - n, 0)
+
+        self.set_focus(new_focus)
 
     def mouse_event(self, size, event, button, col, row, focus):
         button_map = {
@@ -29,7 +46,11 @@ class ScrollingListBox(urwid.ListBox):
     def keypress(self, size, key):
         key_map = {
             'j': self.scroll_down,
-            'k': self.scroll_up
+            'k': self.scroll_up,
+            'ctrl d': self.page_down,
+            'ctrl u': self.page_up,
+            'g': self.top,
+            'G': self.bottom,
         }
 
         if key in key_map:
